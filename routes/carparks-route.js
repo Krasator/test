e21dd95f0bff4c77a0ac74d2954c4e66ffe42fec
krasator <tsiolkas@gmail.com>
2015-02-19 13:27:12 +0200

    Minor fixes

--- routes/carparks-route.js
+++ routes/carparks-route.js
@@ -9,7 +9,7 @@ var CarPark = mongoose.model('CarPark');
 // Get carParks
 router.get('/', function(req, res) {
     CarPark.find(function(err, carParks) {
-        if (err) return res.json('Could not get car parks.');
+        if (err) return res.status(500).json('Could not get car parks.');
         return res.json(carParks);
     });
 });
@@ -18,8 +18,24 @@ router.get('/', function(req, res) {
 // Get carPark
 router.get('/:id', function(req, res) {
     var carParkId = req.params.id;
-
-    return res.json('Hello, you used GET /' + pollId);
+    CarPark.findOne( { 'id': carParkId }, function(err, carPark) {
+        if (err) return res.json('Could not get car park with id ' + id);
+        return res.json(carPark);
+        // if (carParkId !== 1) {
+        //     return res.json(carPark);
+        // }
+        // connectToSql(function(err) {
+        //     if (err) return res.status(500).json('Could not connect to SQL', err);
+        //     sql.query('SELECT * FROM XXX', function(err, avail) {
+        //         if (err) return res.status(500).json('Could not get available parking slots', err);
+        //         carPark.parkingSlots = {
+        //             ts: new Date(),
+        //             available: avail 
+        //         };
+        //         return res.json(carPark);
+        //     });
+        // })
+    });
 });
 
 
@@ -27,7 +43,7 @@ router.get('/:id', function(req, res) {
 router.post('/', function(req, res) {
     var carPark = new CarPark(req.body);
     carPark.save(function(err) {
-        if (err) return res.json('You made a mistake.' + err);
+        if (err) return res.status(500).json('You made a mistake.' + err);
         return res.json(carPark);
     });
 });
